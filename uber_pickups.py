9186391a09d054fdbadaa2da2e67a898488f9036
Esuabom Dijemeni <e.dijemeni@gmail.com>
2025-06-11 10:27:23 +0100

    Add an histogram

--- uber_pickups.py
+++ uber_pickups.py
@@ -11,6 +11,7 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 DATA_FILE = 'uber-raw-data-sep14.csv'
 
+@st.cache_data
 def load_data(nrows):
     data = pd.read_csv(DATA_FILE, nrows=nrows)
     lowercase = lambda x: str(x).lower()
@@ -18,7 +19,16 @@ def load_data(nrows):
     data[DATA_COLUMN] = pd.to_datetime(data[DATA_COLUMN])
     return data
 
+# Effortless caching
 data_load_state = st.text('Loading data... ')
 data = load_data(10000)
 data_load_state.text("Done! (using st.cache_data)")
-data
+
+# Inspect data
+st.header('Raw data')
+st.write(data)
+
+# Draw a histogram
+st.header("Number of pickups by hour")
+hist_values = np.histogram(data[DATA_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
