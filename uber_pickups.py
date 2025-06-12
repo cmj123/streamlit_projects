80b9e57686ee6196bd9990b19c8f9c31dfea95b4
Esuabom Dijemeni <e.dijemeni@gmail.com>
2025-06-12 08:24:43 +0100

    Plot data on a map

--- uber_pickups.py
+++ uber_pickups.py
@@ -6,7 +6,7 @@ import numpy as np
 st.title('Uber pickups in NYC')
 
 # Fetch some data
-DATA_COLUMN = 'date/time'
+DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 DATA_FILE = 'uber-raw-data-sep14.csv'
@@ -16,7 +16,7 @@ def load_data(nrows):
     data = pd.read_csv(DATA_FILE, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
-    data[DATA_COLUMN] = pd.to_datetime(data[DATA_COLUMN])
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
 # Effortless caching
@@ -30,5 +30,15 @@ st.write(data)
 
 # Draw a histogram
 st.header("Number of pickups by hour")
-hist_values = np.histogram(data[DATA_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
+
+
+## Plot data on a map
+# st.subheader('Map of all pickups')
+# st.map(data)
+
+hour_to_filter = 17
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.map(filtered_data)
